1d53a244304cc4ac7d80e0d7c240f5023662414a
CharandeepSingh01 <login.charandeep@gmail.com>
2023-07-17 02:32:44 +0530

    modifying new elements

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -185,191 +185,86 @@ const Dashboard = () => {
     </div>
 </section>
 
-<div class="w-full p-12 bg-white">
-    <div class="flex items-end justify-between mb-12 header">
-        <div class="title">
-            <p class="mb-4 text-4xl font-bold text-gray-800">
-                Lastest articles
+<div class="text-gray-900 pt-12 pr-0 pb-14 pl-0 bg-white">
+  <div class="w-full pt-4 pr-5 pb-6 pl-5 mt-0 mr-auto mb-0 ml-auto space-y-5 sm:py-8 md:py-12 sm:space-y-8 md:space-y-16
+      max-w-7xl">
+    <div class="flex flex-col items-center sm:px-5 md:flex-row">
+      <div class="flex flex-col items-start justify-center w-full h-full pt-6 pr-0 pb-6 pl-0 mb-6 md:mb-0 md:w-1/2">
+        <div class="flex flex-col items-start justify-center h-full space-y-3 transform md:pr-10 lg:pr-16
+            md:space-y-5">
+          <div class="bg-green-500 flex items-center leading-none rounded-full text-gray-50 pt-1.5 pr-3 pb-1.5 pl-2
+              uppercase inline-block">
+            <p class="inline">
+              <svg class="w-3.5 h-3.5 mr-1" fill="currentColor" viewbox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0
+                  00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755
+                  1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1
+                  0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/></svg>
             </p>
-           
+            <p class="inline text-xs font-medium">New</p>
+          </div>
+          <a class="text-4xl font-bold leading-none lg:text-5xl xl:text-6xl">Write anything. Be creative.</a>
+          <div class="pt-2 pr-0 pb-0 pl-0">
+            <p class="text-sm font-medium inline">author:</p>
+            <a class="inline text-sm font-medium mt-0 mr-1 mb-0 ml-1 underline">Jack Sparrow</a>
+            <p class="inline text-sm font-medium mt-0 mr-1 mb-0 ml-1">· 23rd, April 2021 ·</p>
+            <p class="text-gray-200 text-sm font-medium inline mt-0 mr-1 mb-0 ml-1">1hr 20min. read</p>
+          </div>
         </div>
+      </div>
+      <div class="w-full md:w-1/2">
+        <div class="block">
+          <img
+              src="https://images.unsplash.com/photo-1626314928277-1d373ddb6428?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mzd8fHxlbnwwfHx8fA%3D%3D&amp;auto=format&amp;fit=crop&amp;w=500&amp;q=60" class="object-cover rounded-lg max-h-64 sm:max-h-96 btn- w-full h-full"/>
         </div>
-        
-        <div class="grid grid-cols-1 gap-12 md:grid-cols-2 xl:grid-cols-3">
-            <div class="m-auto overflow-hidden rounded-lg shadow-lg cursor-pointer h-90 w-60 md:w-80">
-                <a href="#" class="block w-full h-full">
-                    <img alt="blog photo" src="/images/blog/1.jpg" class="object-cover w-full max-h-40"/>
-                    <div class="w-full p-4 bg-white dark:bg-gray-800">
-                        <p class="font-medium text-indigo-500 text-md">
-                            Video
-                        </p>
-                        <p class="mb-2 text-xl font-medium text-gray-800 dark:text-white">
-                            Work at home
-                        </p>
-                        <p class="font-light text-gray-400 dark:text-gray-300 text-md">
-                            Work at home, remote, is the new age of the job, every person can work at home....
-                        </p>
-                        <div class="flex items-center mt-4">
-                            <a href="#" class="relative block">
-                                <img alt="profil" src="/images/person/6.jpg" class="mx-auto object-cover rounded-full h-10 w-10 "/>
-                            </a>
-                            <div class="flex flex-col justify-between ml-4 text-sm">
-                                <p class="text-gray-800 dark:text-white">
-                                    Jean Jacques
-                                </p>
-                                <p class="text-gray-400 dark:text-gray-300">
-                                    20 mars 2029 - 6 min read
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-            </div>
-            <div class="m-auto overflow-hidden rounded-lg shadow-lg cursor-pointer h-90 w-60 md:w-80">
-                <a href="#" class="block w-full h-full">
-                    <img alt="blog photo" src="/images/blog/2.jpg" class="object-cover w-full max-h-40"/>
-                    <div class="w-full p-4 bg-white dark:bg-gray-800">
-                        <p class="font-medium text-indigo-500 text-md">
-                            Oui
-                        </p>
-                        <p class="mb-2 text-xl font-medium text-gray-800 dark:text-white">
-                            test
-                        </p>
-                        <p class="font-light text-gray-400 dark:text-gray-300 text-md">
-                            The new supercar is here, 543 cv and 140 000$ !!
-                        </p>
-                        <div class="flex items-center mt-4">
-                            <a href="#" class="relative block">
-                                <img alt="profil" src="/images/person/6.jpg" class="mx-auto object-cover rounded-full h-10 w-10 "/>
-                            </a>
-                            <div class="flex flex-col justify-between ml-4 text-sm">
-                                <p class="text-gray-800 dark:text-white">
-                                    Jean Jacques
-                                </p>
-                                <p class="text-gray-400 dark:text-gray-300">
-                                    20 mars 2029 - 6 min read
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-            </div>
-            <div class="m-auto overflow-hidden rounded-lg shadow-lg cursor-pointer h-90 w-60 md:w-80">
-                <a href="#" class="block w-full h-full">
-                    <img alt="blog photo" src="/images/blog/3.jpg" class="object-cover w-full max-h-40"/>
-                    <div class="w-full p-4 bg-white dark:bg-gray-800">
-                        <p class="font-medium text-indigo-500 text-md">
-                            Oui
-                        </p>
-                        <p class="mb-2 text-xl font-medium text-gray-800 dark:text-white">
-                            test
-                        </p>
-                        <p class="font-light text-gray-400 dark:text-gray-300 text-md">
-                            The new supercar is here, 543 cv and 140 000$ !!
-                        </p>
-                        <div class="flex items-center mt-4">
-                            <a href="#" class="relative block">
-                                <img alt="profil" src="/images/person/6.jpg" class="mx-auto object-cover rounded-full h-10 w-10 "/>
-                            </a>
-                            <div class="flex flex-col justify-between ml-4 text-sm">
-                                <p class="text-gray-800 dark:text-white">
-                                    Jean Jacques
-                                </p>
-                                <p class="text-gray-400 dark:text-gray-300">
-                                    20 mars 2029 - 6 min read
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-            </div>
-            <div class="m-auto overflow-hidden rounded-lg shadow-lg cursor-pointer h-90 w-60 md:w-80">
-                <a href="#" class="block w-full h-full">
-                    <img alt="blog photo" src="/images/blog/4.jpg" class="object-cover w-full max-h-40"/>
-                    <div class="w-full p-4 bg-white dark:bg-gray-800">
-                        <p class="font-medium text-indigo-500 text-md">
-                        </p>
-                        <p class="mb-2 text-xl font-medium text-gray-800 dark:text-white">
-                            test
-                        </p>
-                        <p class="font-light text-gray-400 dark:text-gray-300 text-md">
-                            The new supercar is here, 543 cv and 140 000$ !!
-                        </p>
-                        <div class="flex items-center mt-4">
-                            <a href="#" class="relative block">
-                                <img alt="profil" src="/images/person/6.jpg" class="mx-auto object-cover rounded-full h-10 w-10 "/>
-                            </a>
-                            <div class="flex flex-col justify-between ml-4 text-sm">
-                                <p class="text-gray-800 dark:text-white">
-                                    Jean Jacques
-                                </p>
-                                <p class="text-gray-400 dark:text-gray-300">
-                                    20 mars 2029 - 6 min read
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-            </div>
-            <div class="m-auto overflow-hidden rounded-lg shadow-lg cursor-pointer h-90 w-60 md:w-80">
-                <a href="#" class="block w-full h-full">
-                    <img alt="blog photo" src="/images/blog/5.jpg" class="object-cover w-full max-h-40"/>
-                    <div class="w-full p-4 bg-white dark:bg-gray-800">
-                        <p class="font-medium text-indigo-500 text-md">
-                        </p>
-                        <p class="mb-2 text-xl font-medium text-gray-800 dark:text-white">
-                            test
-                        </p>
-                        <p class="font-light text-gray-400 dark:text-gray-300 text-md">
-                            The new supercar is here, 543 cv and 140 000$ !!
-                        </p>
-                        <div class="flex items-center mt-4">
-                            <a href="#" class="relative block">
-                                <img alt="profil" src="/images/person/6.jpg" class="mx-auto object-cover rounded-full h-10 w-10 "/>
-                            </a>
-                            <div class="flex flex-col justify-between ml-4 text-sm">
-                                <p class="text-gray-800 dark:text-white">
-                                    Jean Jacques
-                                </p>
-                                <p class="text-gray-400 dark:text-gray-300">
-                                    20 mars 2029 - 6 min read
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-            </div>
-            <div class="m-auto overflow-hidden rounded-lg shadow-lg cursor-pointer h-90 w-60 md:w-80">
-                <a href="#" class="block w-full h-full">
-                    <img alt="blog photo" src="/images/blog/6.jpg" class="object-cover w-full max-h-40"/>
-                    <div class="w-full p-4 bg-white dark:bg-gray-800">
-                        <p class="font-medium text-indigo-500 text-md">
-                            Oui
-                        </p>
-                        <p class="mb-2 text-xl font-medium text-gray-800 dark:text-white">
-                            test
-                        </p>
-                        <p class="font-light text-gray-400 dark:text-gray-300 text-md">
-                            The new supercar is here, 543 cv and 140 000$ !!
-                        </p>
-                        <div class="flex items-center mt-4">
-                            <a href="#" class="relative block">
-                                <img alt="profil" src="/images/person/6.jpg" class="mx-auto object-cover rounded-full h-10 w-10 "/>
-                            </a>
-                            <div class="flex flex-col justify-between ml-4 text-sm">
-                                <p class="text-gray-800 dark:text-white">
-                                    Jean Jacques
-                                </p>
-                                <p class="text-gray-400 dark:text-gray-300">
-                                    20 mars 2029 - 6 min read
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-            </div>
-        </div>
+      </div>
     </div>
+    <div class="grid grid-cols-12 sm:px-5 gap-x-8 gap-y-16">
+      <div class="flex flex-col items-start col-span-12 space-y-3 sm:col-span-6 xl:col-span-4">
+        <img
+            src="https://images.unsplash.com/photo-1626318305863-bb23d0297c0b?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=500&amp;q=60" class="object-cover w-full mb-2 overflow-hidden rounded-lg shadow-sm max-h-56 btn-"/>
+        <p class="bg-green-500 flex items-center leading-none text-sm font-medium text-gray-50 pt-1.5 pr-3 pb-1.5 pl-3
+            rounded-full uppercase inline-block">Entertainment</p>
+        <a class="text-lg font-bold sm:text-xl md:text-2xl">Improving your day to the MAX</a>
+        <p class="text-sm text-black">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+        <div class="pt-2 pr-0 pb-0 pl-0">
+          <a class="inline text-xs font-medium mt-0 mr-1 mb-0 ml-0 underline">Jack Sparrow</a>
+          <p class="inline text-xs font-medium mt-0 mr-1 mb-0 ml-1">· 23rd, March 2021 ·</p>
+          <p class="inline text-xs font-medium text-gray-300 mt-0 mr-1 mb-0 ml-1">1hr 20min. read</p>
+        </div>
+      </div>
+      <div class="flex flex-col items-start col-span-12 space-y-3 sm:col-span-6 xl:col-span-4">
+        <img
+            src="https://images.unsplash.com/photo-1626285861696-9f0bf5a49c6d?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTl8fHxlbnwwfHx8fA%3D%3D&amp;auto=format&amp;fit=crop&amp;w=500&amp;q=60" class="object-cover w-full mb-2 overflow-hidden rounded-lg shadow-sm max-h-56 btn-"/>
+        <p class="bg-green-500 flex items-center leading-none text-sm font-medium text-gray-50 pt-1.5 pr-3 pb-1.5 pl-3
+            rounded-full uppercase inline-block">Entertainment</p>
+        <a class="text-lg font-bold sm:text-xl md:text-2xl">Improving your day to the MAX</a>
+        <p class="text-sm text-black">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+        <div class="pt-2 pr-0 pb-0 pl-0">
+          <a class="inline text-xs font-medium mt-0 mr-1 mb-0 ml-0 underline">Jack Sparrow</a>
+          <p class="inline text-xs font-medium mt-0 mr-1 mb-0 ml-1">· 23rd, March 2021 ·</p>
+          <p class="inline text-xs font-medium text-gray-300 mt-0 mr-1 mb-0 ml-1">1hr 20min. read</p>
+        </div>
+      </div>
+      <div class="flex flex-col items-start col-span-12 space-y-3 sm:col-span-6 xl:col-span-4">
+        <img
+            src="https://images.unsplash.com/photo-1626197031507-c17099753214?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MzR8fHxlbnwwfHx8fA%3D%3D&amp;auto=format&amp;fit=crop&amp;w=500&amp;q=60" class="object-cover w-full mb-2 overflow-hidden rounded-lg shadow-sm max-h-56 btn-"/>
+        <p class="bg-green-500 flex items-center leading-none text-sm font-medium text-gray-50 pt-1.5 pr-3 pb-1.5 pl-3
+            rounded-full uppercase inline-block">Entertainment</p>
+        <a class="text-lg font-bold sm:text-xl md:text-2xl">Improving your day to the MAX</a>
+        <p class="text-sm text-black">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+        <div class="pt-2 pr-0 pb-0 pl-0">
+          <a class="inline text-xs font-medium mt-0 mr-1 mb-0 ml-0 underline">Jack Sparrow</a>
+          <p class="inline text-xs font-medium mt-0 mr-1 mb-0 ml-1">· 23rd, March 2021 ·</p>
+          <p class="inline text-xs font-medium text-gray-300 mt-0 mr-1 mb-0 ml-1">1hr 20min. read</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
     
 <section class="bg-indigo-800 px-8">
@@ -421,7 +316,7 @@ const Dashboard = () => {
                 
             </h5>
             <p class="text-xs font-medium tracking-wide text-indigo-100 uppercase">
-                Years experience
+                Year experience
             </p>
         </div>
     </div>
@@ -429,7 +324,7 @@ const Dashboard = () => {
 </section>
 
 <section class="max-w-screen-xl px-4 py-12 mx-auto bg-gray-900 dark:bg-gray-800 sm:py-16 sm:px-6 lg:px-8 lg:py-20 mt-4">
-    <div class="max-w-4xl mx-auto text-center">
+    <div class="max-w-4xl mx-auto text-center px-8">
         <h2 class="text-3xl font-extrabold leading-9 text-white sm:text-4xl sm:leading-10">
             I am always up for the opportunity to work with others.
         </h2>
@@ -437,7 +332,7 @@ const Dashboard = () => {
             Feel confident in choosing me as the best candidate for your tech needs.
         </p>
     </div>
-    <div class="mt-10 text-center sm:max-w-3xl sm:mx-auto sm:grid sm:grid-cols-1 sm:gap-8">
+    <div class="mt-10 text-center sm:max-w-3xl sm:mx-auto sm:grid sm:grid-cols-1 sm:gap-8 px-8">
         <div>
             <p class="text-5xl font-extrabold leading-none text-white">
                 To get in touch
